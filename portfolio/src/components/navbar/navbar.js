18001354d12e313f1f55fc6424bb47e8ed9b7e0a
CodeNode21 <mbednara@gmail.com>
2019-11-12 21:54:54 -0500

    added React option

---
+++ portfolio/src/components/navbar/navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-light">
+              <a className="navbar-brand" href="#">
+                  <img src="../../mbWnB.png" width="30" height="30" alt="Logo"/>
+              </a>
+            </nav>
+        </div>
+    );
+}
+
+export default NavBar;
